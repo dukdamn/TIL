4abbca12c898d8094389e7ad294021875825fcf1
dukdamn <dukdamn0927@gmail.com>
2019-11-15 16:54:45 +0900

    Add file

---
+++ swift/2019.11.14/homework.playground/Contents.swift
@@ -0,0 +1,119 @@
+import UIKit
+
+//[ 과제 ]
+//1. 옵셔널 타입의 문자열 파라미터 3개를 입력받은 뒤, 옵셔널을 추출하여 Unwrapped 된 하나의 문자열로 합쳐서 반환하는 함수
+//func combineString(str1: String?, str2: String?, str3: String?) -> String {
+//  // code
+//}
+//// 입력 예시 및 결과
+//combineString1(str1: "AB", str2: "CD", str3: "EF")   // "ABCDEF"
+//combineString1(str1: "AB", str2: nil, str3: "EF")    // "ABEF"
+
+func combineString(str1: String?, str2: String?, str3: String?) -> String {
+    var duck: String = ""
+    if let check = str1{
+        duck.append(contentsOf: check)
+    }
+     if let check = str2{
+        duck.append(contentsOf: check)
+    }
+    if let check = str3{
+        duck.append(contentsOf: check)
+    }
+    return duck
+}
+combineString(str1: nil, str2: "CD", str3: "EF")
+
+
+//2. 사칙연산(+, -, *, /)을 가진 enum 타입 Arithmetic과 2개의 자연수를 입력 파라미터로 받아 (파라미터 총 3개) 해당 연산의 결과를 반환하는 함수 구현
+//enum Arithmetic {
+//  case addition, subtraction, multiplication, division
+//}
+
+enum Arithmetic {
+    case addition, subtraction, multiplication, division
+    func operate(_ num1: Double,_ num2: Double) -> Double {
+        switch self {
+        case .addition:
+            return num1 + num2
+        case .subtraction:
+            return num1 - num2
+        case .multiplication:
+            return num1 * num2
+        case .division:
+            return num1 / num2
+        }
+    }
+}
+var duck = Arithmetic.addition
+duck.operate(3, 3)
+
+//[ 도전 과제 ]
+//1. celcius, fahrenheit, kelvin 온도 3가지 케이스를 가진 enum 타입 Temperature 를 정의
+//각 케이스에는 Double 타입의 Associated Value 를 받도록 함
+//추가로 Temperature 타입 내부에 각 온도를 섭씨 온도로 변환해주는 toCelcius() 함수를 구현
+//섭씨 = (화씨 - 32) * 5 / 9
+//섭씨 = 켈빈 + 273
+//
+//enum Temperature {
+//  // 코드
+//} 섭씨 화씨 절대
+enum Temperature {
+    case celcius(Double)
+    case fahrenheit(Double)
+    case kelvinv(Double)
+    
+    func toCelcius()->Double{
+        switch self {
+        case .celcius(let x): return x
+        case .fahrenheit(let x): return (x - 32) * 5 / 9
+        case .kelvinv(let x): return x + 273
+        }
+    }
+}
+
+var duck1: Temperature = .fahrenheit(43)
+print(duck1.toCelcius())
+
+
+//2. 다음 ArithmeticExpression 의 각 케이스별로 연산을 수행하고 그 값을 반환하는 evaluate 함수 구현
+//enum ArithmeticExpression {
+//  case number(Int)
+//  case addition(ArithmeticExpression, ArithmeticExpression)
+//  case multiplication(ArithmeticExpression, ArithmeticExpression)
+//}
+//let five = ArithmeticExpression.number(5)
+//let four = ArithmeticExpression.number(4)
+//let sum = ArithmeticExpression.addition(five, four)
+//let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+//func evaluate(_ expression: ArithmeticExpression) -> Int {
+//  // 코드
+//}
+//evaluate(five)    // 결과 : 5
+//evaluate(sum)     // 결과 : 9
+//evaluate(product) // 결과 : 18
+
+
+
+indirect enum ArithmeticExpression {
+  case number(Int)
+  case addition(ArithmeticExpression, ArithmeticExpression)
+  case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case .number(let x): return x
+    case .addition(let x, let y):
+        return evaluate(x) + evaluate(y)
+    case .multiplication(let x, let y):
+        return evaluate(x) * evaluate(y)
+    }
+}
+
+evaluate(five)    // 결과 : 5
+evaluate(sum)     // 결과 : 9
+evaluate(product) // 결과 : 18
