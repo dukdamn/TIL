2dbc37e39276a0385dabc1a43b441c643708ca99
dukdamn <dukdamn0927@gmail.com>
2019-11-18 12:46:57 +0900

    Update file

--- swift/2019.11.15/homework/homework.playground/Contents.swift
+++ swift/2019.11.15/homework/homework.playground/Contents.swift
@@ -7,18 +7,18 @@ import UIKit
 
 //1. 아래 두 클로저를 Syntax Optimization을 이용하여 최대한 줄여보기let someClosure: (String, String) -> Bool = { (s1: String, s2: String) -> Bool in
 let someClosure: (String, String) -> Bool = { (s1: String, s2: String) -> Bool in
-  let isAscending: Bool
-  if s1 > s2 {
-    isAscending = true
-  } else {
-    isAscending = false
-  }
-  return isAscending
+    let isAscending: Bool
+    if s1 > s2 {
+        isAscending = true
+    } else {
+        isAscending = false
+    }
+    return isAscending
 }
 let otherClosure: ([Int]) -> Int = { (values: [Int]) -> Int in
-  var count: Int = 0
-  for _ in values {
-    count += 1
-  }
-  return count
+    var count: Int = 0
+    for _ in values {
+        count += 1
+    }
+    return count
 }
