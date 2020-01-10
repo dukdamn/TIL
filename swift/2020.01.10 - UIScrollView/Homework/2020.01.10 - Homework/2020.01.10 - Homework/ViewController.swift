c7930ea8e342932f4e230d4674570e26d96c8856
dukdamn <dukdamn0927@gmail.com>
2020-01-10 18:34:37 +0900

    update

---
+++ swift/2020.01.10 - UIScrollView/Homework/2020.01.10 - Homework/2020.01.10 - Homework/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  2020.01.10 - Homework
+//
+//  Created by 황정덕 on 2020/01/10.
+//  Copyright © 2020 Gitbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var scrollView: UIScrollView!
+  var pageControl = UIPageControl()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    scrollView.delegate = self
+    scrollView.isPagingEnabled = true
+    scrollView.alwaysBounceVertical = false
+    
+    
+    pageControl.frame = CGRect(x: view.frame.midX - 50, y: view.frame.maxY - 150, width: 100, height: 50)
+    pageControl.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+    pageControl.isUserInteractionEnabled = false
+//    pageControl.currentPage = 0
+//
+    pageControl.numberOfPages = 4
+    view.addSubview(pageControl)
+  }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // floor 내림, ceil 올림
+        // contentOffset는 현재 스크롤된 좌표
+        pageControl.currentPage = Int(floor(scrollView.contentOffset.x / UIScreen.main.bounds.width))
+      
+//      pageControl.currentPage = Int(floor(scrollView.contentOffset.x / UIScreen.main.bounds.width))
+    }
+}
--- swift/2020.01.10 - UIScrollView/강의자료/ScrollViewExample - Starter/ScrollViewExample/ViewController.swift
+++ swift/2020.01.10 - UIScrollView/강의자료/ScrollViewExample - Starter/ScrollViewExample/ViewController.swift
@@ -16,24 +16,72 @@ final class ViewController: UIViewController {
   @IBOutlet private weak var scrollView: UIScrollView!
   @IBOutlet private weak var imageView: UIImageView!
   
+  
+  private var zoomScale: CGFloat = 1.0
+//  {
+//    didset {
+//      print("current zoom Scale :", String(format: "%.2", zoomScale))
+//    }
+//  }
+  
   // MARK: LifeCycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    scrollView.delegate = self
+    scrollView.bounces = true
+    // 확대가능
+    scrollView.alwaysBounceVertical = true
+    updateScrollViewZoomScale()
+  }
+  
+  private func updateScrollViewZoomScale(){
+    let widthScale = view.frame.width / imageView.bounds.width
+    let heightScale = view.frame.height / imageView.bounds.height
+    let minScale = min(widthScale, heightScale)
+    
+    // 더 높은값을 다른데 주더라도 맥스와 민을 주어서 그만큼 커지거나 내려가지 않음
+    scrollView.minimumZoomScale = minScale
+    scrollView.zoomScale = 1
+    scrollView.maximumZoomScale = 3
   }
   
   // MARK: Action Handler
   
   @IBAction private func fitToWidth(_ sender: Any) {
     print("\n---------- [ fitToWidth ] ----------")
+    zoomScale = scrollView.frame.width / imageView.bounds.width
+    print(scrollView.frame.width)
+    print(imageView.bounds.width)
+    print(zoomScale)
+    scrollView.setZoomScale(zoomScale, animated: true)
   }
   
   @IBAction private func scaleDouble(_ sender: Any) {
     print("\n---------- [ scaleDouble ] ----------")
+    scrollView.setZoomScale(zoomScale * 2, animated: true)
+    zoomScale = scrollView.zoomScale
+    
+    // 이 위치에 보여줘라
+    let rectToVisble = CGRect(x: 100, y: 100, width: 200, height: 300)
+    scrollView.zoom(to: rectToVisble, animated: true)
   }
 
   @IBAction private func moveContentToLeft(_ sender: Any) {
     print("\n---------- [ moveContentToLeft ] ----------")
+    let newOffset = CGPoint(x: scrollView.contentOffset.x + 150,
+                            y: scrollView.contentOffset.y)
+    scrollView.setContentOffset(newOffset, animated: true)
+  }
+  
+  
+}
+
+
+extension ViewController: UIScrollViewDelegate {
+  func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+    imageView
   }
 }
+
 
