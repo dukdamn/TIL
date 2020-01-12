e2a0c0946c454b1b24568c4d0d70f31cf0064db6
dukdamn <dukdamn0927@gmail.com>
2020-01-13 03:35:28 +0900

    새 메모

---
+++ Kxcoding/MemoApp/MemoApp/ComposeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ComposeViewController.swift
+//  MemoApp
+//
+//  Created by 황정덕 on 2020/01/13.
+//  Copyright © 2020 Gitbot. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    print("\n----------[새로운메모]----------\n")
+    // Do any additional setup after loading the view.
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
