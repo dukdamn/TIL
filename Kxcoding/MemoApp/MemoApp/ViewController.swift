b49ae0c8acd3b0ded594ffbc4e95642554d28d82
dukdamn <dukdamn0927@gmail.com>
2020-01-13 02:09:34 +0900

    프로젝트 생성

---
+++ Kxcoding/MemoApp/MemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by 황정덕 on 2020/01/13.
+//  Copyright © 2020 Gitbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
